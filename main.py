67924760333f5a7e82fb9eff320b34dd3290b158
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-06-18 00:05:40 -0300

    add custom error pages

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ bootstrap = Bootstrap(app)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.errorhandler(404)
+def not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def not_found(e):
+    return render_template('500.html'), 500
