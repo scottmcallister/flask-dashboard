f42c2a71b0b756d19c29380d2c96c7648e4c917a
Scott McAllister <mr.scott.mcallister@gmail.com>
2018-06-17 21:23:17 -0300

    add bootstrap and create template directory

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
